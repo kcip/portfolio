aaf4e1df3ed86e549909451e8e8d87d59a45c53a
Scott <scottepick@mac.com>
2020-08-03 16:52:30 -0400

    mobile nav and styles

--- src/shared/Nav.jsx
+++ src/shared/Nav.jsx
@@ -3,26 +3,26 @@ import { NavLink } from "react-router-dom";
 import useToggle from '../hooks/useToggle'
 import './nav.scss';
 const Nav = () => {
-  const [isVisible, toggleisVisible] = useToggle(true);
-
-  const show = () => {
+  const [isVisible, toggleisVisible] = useToggle(false);
+  const [open, setOpen] = useToggle(false)
+  const [show, setShow] = useToggle(false)
+  const hamburger = () => {
+    setOpen()
     toggleisVisible()
+    setShow()
   }
+
+
   return (
     <Fragment>
-      <div className="mobile--nav" onClick={show} >
-        <span className="mobile--nav-span"></span>
+      <div className="mobile--nav" onClick={() => hamburger()} >
+        <span className={open ? "mobile--nav-span menu-is-open" : "mobile--nav-span"}></span>
       </div>
-      <nav className="nav" style={{ display: isVisible ? 'flex' : 'none' }}>
-        {/* <Fragment> */}
-        {/* <div className="mobile--nav" onClick={show} >
-          <span className="mobile--nav-span"></span>
-        </div> */}
-
-        <NavLink activeClassName="activeLink" to="/" exact>Home</NavLink>
-        <NavLink activeClassName="activeLink" to="/projects" exact>Projects</NavLink>
-        <NavLink activeClassName="activeLink" to="/about" exact>About me</NavLink>
-        {/* <NavLink activeClassName="activeLink" to="/contact" exact>Contact</NavLink> */}
+      <nav className={isVisible ? "nav show" : "nav"}>
+        {/* <nav className="nav" style={{ transform: isVisible ? 'translateX(0%)' : 'translateX(110%)' }}> */}
+        <NavLink className={show ? 'is-shown' : null} activeClassName="activeLink" to="/" exact>Home</NavLink>
+        <NavLink className={show ? 'is-shown' : null} activeClassName="activeLink" to="/projects" exact>Projects</NavLink>
+        <NavLink className={show ? 'is-shown' : null} activeClassName="activeLink" to="/about" exact>About me</NavLink>
       </nav>
     </Fragment>
 
